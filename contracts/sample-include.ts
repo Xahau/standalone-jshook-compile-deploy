410f683eb4f9f90219f02a378e619d1f81aa67ef
Wietse Wind <mail@wietse.com>
2024-06-05 11:02:22 +0200

    Update sample-include.ts

--- contracts/sample-include.ts
+++ contracts/sample-include.ts
@@ -1,22 +1,12 @@
-import {
-  enableEmit,
-  prepareEmit,
-  doEmit,
-  log,
-} from '@xahau/hooks'
+import { enableEmit, prepareEmit, doEmit, log, } from '@xahau/hooks'
 
 export const sampleIncludeEmitter = () => {
   enableEmit(10)
 
   const prepared = prepareEmit({
     TransactionType: 'Payment',
-    Amount: '3000000',
-    Memos: [{
-      Memo: {
-        MemoType: 'DEAD',
-        MemoData: 'BEEF'
-      }
-    }],
+    Amount: '3000000', // 3mm drops = 3 XAH
+    Memos: [ { Memo: { MemoType: 'DEAD', MemoData: 'BEEF' } } ],
     Destination: 'rwietsevLFg8XSmG3bEZzFein1g8RBqWDZ',
   })
 
